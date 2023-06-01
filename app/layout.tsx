10d40a33d29908c7be0877c4d169b3bf00c2a06e
Esto Triramdani N <estolagi@gmail.com>
2023-06-01 14:29:07 +0700

    setup proivders

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import { Inter } from 'next/font/google';
 import SupabaseProvider from '@/providers/SupabaseProviders';
 import { Metadata } from 'next';
+import UserProvider from '@/providers/UserProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,7 +17,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={inter.className}>
         <SupabaseProvider>
-          <Sidebar>{children}</Sidebar>
+          <UserProvider>
+            <Sidebar>{children}</Sidebar>
+          </UserProvider>
         </SupabaseProvider>
       </body>
     </html>
---
+++ providers/UserProvider.tsx
@@ -0,0 +1,11 @@
+import { MyUserContextProvider } from '@/hooks/useUser';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const UserProvider: React.FC<Props> = ({ children }) => {
+  return <MyUserContextProvider>{children}</MyUserContextProvider>;
+};
+
+export default UserProvider;
