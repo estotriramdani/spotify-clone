d362af62971b140777c62fc64e0017c04d79b799
Esto Triramdani N <estolagi@gmail.com>
2023-06-01 10:43:15 +0700

    sidebar items

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,9 +2,12 @@
 
 import React, { useMemo } from 'react';
 
-import { usePathname, useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import { HiHome } from 'react-icons/hi';
 import { BiSearch } from 'react-icons/bi';
+
+import Box from './Box';
+import SidebarItem from './SidebarItem';
 
 interface SidebarProps {
   children: React.ReactNode;
@@ -16,13 +19,13 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
   const routes = useMemo(
     () => [
       {
-        icons: HiHome,
+        icon: HiHome,
         label: 'Home',
         active: pathname !== '/search',
         href: '/',
       },
       {
-        icons: BiSearch,
+        icon: BiSearch,
         label: 'Search',
         active: pathname === '/search',
         href: '/search',
@@ -31,7 +34,36 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
     [pathname]
   );
 
-  return <div className='flex h-full'>{children}</div>;
+  return (
+    <div className="flex h-full">
+      <div
+        className="
+      hidden
+      md:flex
+      flex-col
+      gap-y-2
+      bg-black
+      h-full
+      w-[300px]
+      p-2
+    "
+      >
+        <Box>
+          <div className='flex flex-col gap-y-4 px-5 py-4'>
+            {
+              routes.map(item => (
+                <SidebarItem
+                  key={item.label}
+                  {...item}
+                />
+              ))
+            }
+          </div>
+        </Box>
+        <Box className="overflow-y-auto h-full">Song Library</Box>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
